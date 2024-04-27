895dc6aa8492d6ab5288a78926e56e51b4e8494d
tgparkk <sttgpark@gmail.com>
2024-04-27 16:16:55 +0900

    1

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Markup: Syntax Highlighting",
+        "excerpt":"Syntax highlighting is a feature that displays source code, in different colors and fonts according to the category of terms. This feature facilitates writing in a structured language such as a programming language or a markup language as both structures and syntax errors are visually distinct. Highlighting does not affect...","categories": [],
+        "tags": ["code","syntax highlighting"],
+        "url": "/markup-syntax-highlighting/",
+        "teaser": "/assets/images/markup-syntax-highlighting-teaser.jpg"
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["Jekyll"],
+        "tags": ["update"],
+        "url": "/jekyll/welcome-to-jekyll/",
+        "teaser": "https://farm5.staticflickr.com/4076/4940499208_b79b77fb0a_z.jpg"
+      },{
+        "title": "Layout: Header Video",
+        "excerpt":"This post should display a header with a responsive video, if the theme supports it. Settings Parameter Required Description id Required ID of the video provider Required Hosting provider of the video, either youtube or vimeo YouTube To embed the following YouTube video at url https://www.youtube.com/watch?v=-PVofD2A9t8 (long version) or https://youtu.be/-PVofD2A9t8...","categories": ["Layout","Uncategorized"],
+        "tags": ["video","layout"],
+        "url": "/layout/uncategorized/layout-header-video/",
+        "teaser": null
+      }]
