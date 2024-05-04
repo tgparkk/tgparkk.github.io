be838bfca1f3b81e0f05b598895537dbb50d3002
tgparkk <sttgpark@gmail.com>
2024-05-04 19:51:22 +0900

    mssql 에서 파일 전송 및 압축 해제

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,16 @@
 var store = [{
+        "title": "Layout: Sidebar Custom",
+        "excerpt":"This post has a custom sidebar set in the post’s YAML Front Matter.   An example of how that YAML could look is:   sidebar:   - title: \"Title\"     image: http://placehold.it/350x250     image_alt: \"image\"     text: \"Some text here.\"   - title: \"Another Title\"     text: \"More text here.\"  ","categories": [],
+        "tags": [],
+        "url": "/layout-sidebar-custom/",
+        "teaser": null
+      },{
+        "title": "Layout: Sidebar with Navigation List",
+        "excerpt":"This post has a custom navigation list set in the post’s YAML Front Matter. sidebar: title: \"Sample Title\" nav: sidebar-sample Along with navigation elements set in _data/navigation.yml. sidebar-sample: - title: \"Parent Page A\" children: - title: \"Child Page A1\" url: / - title: \"Child Page A2\" url: / - title:...","categories": [],
+        "tags": [],
+        "url": "/layout-sidebar-nav-list/",
+        "teaser": null
+      },{
         "title": "Markup: Syntax Highlighting",
         "excerpt":"Syntax highlighting is a feature that displays source code, in different colors and fonts according to the category of terms. This feature facilitates writing in a structured language such as a programming language or a markup language as both structures and syntax errors are visually distinct. Highlighting does not affect...","categories": [],
         "tags": ["code","syntax highlighting"],
@@ -15,5 +27,11 @@ var store = [{
         "excerpt":"This post should display a header with a responsive video, if the theme supports it. Settings Parameter Required Description id Required ID of the video provider Required Hosting provider of the video, either youtube or vimeo YouTube To embed the following YouTube video at url https://www.youtube.com/watch?v=-PVofD2A9t8 (long version) or https://youtu.be/-PVofD2A9t8...","categories": ["Layout","Uncategorized"],
         "tags": ["video","layout"],
         "url": "/layout/uncategorized/layout-header-video/",
+        "teaser": null
+      },{
+        "title": "mssql 에서 파일 압축과 전송",
+        "excerpt":" ","categories": ["data-engineering","mssql"],
+        "tags": [],
+        "url": "/data-engineering/mssql/mssql_data_send_compress/",
         "teaser": null
       }]
